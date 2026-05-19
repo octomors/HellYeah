7f293c06a2de906989d40515a039f4e3198c1fa8
tongzhi <konushkina.diana@gmail.com>
2026-05-19 15:38:39 +0700

    Revert "Cooking screeen design"

--- Assets/Scripts/Cooking/RecipeIngredientIconUI.cs
+++ Assets/Scripts/Cooking/RecipeIngredientIconUI.cs
@@ -13,8 +13,6 @@ public class RecipeIngredientIconUI : MonoBehaviour
     [Header("Colors")]
     [SerializeField] private Color availableColor = Color.white;
     [SerializeField] private Color unavailableColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-    [SerializeField] private Color availableTextColor = Color.white;
-    [SerializeField] private Color unavailableTextColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
 
     public void Setup(Ingredient ingredient, int amount)
     {
@@ -57,7 +55,7 @@ public class RecipeIngredientIconUI : MonoBehaviour
         
         if (amountText != null)
         {
-            amountText.color = available ? availableTextColor : unavailableTextColor;
+            amountText.color = available ? Color.white : Color.red;
         }
     }
 }
